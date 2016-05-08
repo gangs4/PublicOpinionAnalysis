3d3d4da67614457828bd18026777ad277dc69f68
qq519043202 <519043202@qq.com>
2016-05-08 20:55:44 +0800

    last week 0.2.1

--- Application/Home/Controller/NewWeiboController.class.php
+++ Application/Home/Controller/NewWeiboController.class.php
@@ -37,13 +37,15 @@ class NewWeiboController extends BaseController {
         $all_student = $model_student->select();
         $grade_info = $model_student->ShowGrade();
         // dump($all_student);
+        // dump($grade_info);
         $this->display();
     }
     public function in_grade()
     {
         $grade = $_GET['grade'];
         $model_student = D("StudentInfo");
-        $grade_info = $model_student->ShowGrade($grade);
+        $student_info = $model_student->ShowGrade($grade);
+        $grade_info = $model_student->ShowGrade();
         // dump($grade_info);
         $this->display('list_all');
     }
@@ -51,78 +53,10 @@ class NewWeiboController extends BaseController {
     public function detail()
     {
         $id = $_GET['id'];
+        $ldamodel = D("Ldamodel");
+        $lda = $ldamodel->select();
+        // dump($lda);
         $this->display();
     }
-
-//以下是似乎没用了。。。
-
-
-    public function test()
-    {
-        $wordModel = D("WeiboInfo");
-        $arr = $wordModel->search($_GET['str']);
-        dump($arr);
-    }
-
-
-
-    public function search(){
-        if(isset($_GET['type']))
-        {
-            $type = $_GET['type'];
-            $arr_user = null; $arr_cont = null;
-
-            if ($type == 'both' || $type == 'id') {
-                $userModel = D('UserInfo');
-                $arr_user = $userModel->search($_GET['str']);
-            }
-            if ($type == 'both' || $type == 'cont') {
-                $contentModel = D('TotalWeibo');
-                $arr_cont = $contentModel->search($_GET['str']);
-            }
-            $this->assign('data', $arr_user);
-            $this->assign('weibos', $arr_cont);
-            // var_dump($arr_user);
-            // var_dump($arr_cont);
-            $this->display();
-        }
-        else
-        {
-            $this->display();
-        }
-
-    }
-
-    public function personal(){
-        $userModel = D('UserInfo');
-        $arr = $userModel->relation(true)->find($_GET['id']);
-
-        $analysisModel = D('TotalWeibo');
-        $analysis = $analysisModel->analysisPerson($_GET['id']);
-    //var_dump($analysis);
-        $this->assign('hours',json_encode($analysis[0]));
-        $this->assign('weeks',json_encode($analysis[1]));
-        $this->assign('username',json_encode($arr['username']));
-        $this->assign('data',array(0=>$arr));
-        /*var_dump($arr);*/
-        $this->assign('weibos',$arr['weibos']);
-
-        $this->display();
-    }
-
-    // public function detail(){
-    //     $contentModel = D('TotalWeibo');
-    //     if (isset($_GET['id']))
-    //     {
-    //         $arr = $contentModel->searchDetail($_GET['id']);
-    //     }
-    //     else
-    //     {
-    //         $arr = $contentModel->relation(true)->find();
-    //     }
-    //     $arr = array("1"=>$arr);
-    //     $this->assign('weibos',$arr);
-    //     $this->display();
-    // }
 
 }
